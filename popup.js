be8d7f14b348433f7f620807c2aec59755278f07
r-sreesaran <r.sreesaran@gmail.com>
2014-03-31 01:41:56 +0530

    Refactored

--- popup.js
+++ popup.js
@@ -1,34 +1,9 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-/**
- * Global variable containing the query we'd like to pass to Flickr. In this
- * case, kittens!
- *
- * @type {string}
- */
- var QUERY = 'kittens';
+ var QUERY = 'default';
 
  
 var kittenGenerator = {
-  /**
-   * Flickr URL that will give us lots and lots of whatever we're looking for.
-   *
-   * See http://www.flickr.com/services/api/flickr.photos.search.html for
-   * details about the construction of this URL.
-   *
-   * @type {string}
-   * @private
-   */
-  
-  
-  /**
-   * Sends an XHR GET request to grab photos of lots and lots of kittens. The
-   * XHR's 'onload' event is hooks up to the 'showPhotos_' method.
-   *
-   * @public
-   */
+ 
   requestKittens: function() {
     var req = new XMLHttpRequest();
     req.open("GET",'https://secure.flickr.com/services/rest/?' +
@@ -43,14 +18,7 @@ var kittenGenerator = {
     req.send(null);
   },
 
-  /**
-   * Handle the 'onload' event of our kitten XHR request, generated in
-   * 'requestKittens', by generating 'img' elements, and stuffing them into
-   * the document for display.
-   *
-   * @param {ProgressEvent} e The XHR ProgressEvent.
-   * @private
-   */
+  
   showPhotos_: function (e) {
     var kittens = e.target.responseXML.querySelectorAll('photo');
     for (var i = 0; i < kittens.length; i++) {
@@ -64,14 +32,7 @@ var kittenGenerator = {
     }
   },
 
-  /**
-   * Given a photo, construct a URL using the method outlined at
-   * http://www.flickr.com/services/api/misc.urlKittenl
-   *
-   * @param {DOMElement} A kitten.
-   * @return {string} The kitten's URL.
-   * @private
-   */
+  
   constructKittenURL_: function (photo) {
     return "http://farm" + photo.getAttribute("farm") +
         ".static.flickr.com/" + photo.getAttribute("server") +
@@ -81,11 +42,9 @@ var kittenGenerator = {
   }
 };
 
-// Run our kitten generation script as soon as the document's DOM is ready.
 document.addEventListener('DOMContentLoaded', function () {
  
  var button = document.getElementById("click1");
-// Run our kitten generation script as soon as the document's DOM is ready.
  button.addEventListener("click", function () {
   
   if($('div#child').length) {
